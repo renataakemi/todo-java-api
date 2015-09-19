24b201343b479f6e66a051738bc5674da10b11b5
renataakemi <renata.akemi@inatel.br>
2015-09-19 12:20:45 -0300

    Adding service to list tasks

--- src/main/java/br/edu/inatel/task/api/TaskService.java
+++ src/main/java/br/edu/inatel/task/api/TaskService.java
@@ -1,7 +1,30 @@
 package br.edu.inatel.task.api;
 
+import br.edu.inatel.task.model.Task;
+import br.edu.inatel.task.model.TaskDAO;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+
+@Path("/tasks")
 /**
  * Created by Renata Akemi on 19/09/2015.
  */
 public class TaskService {
+
+    TaskDAO taskDAO = new TaskDAO();
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response list(){
+        ArrayList<Task> tasks = taskDAO.list();
+        return Response
+                .ok()
+                .entity(tasks)
+                .build();
+    }
 }
